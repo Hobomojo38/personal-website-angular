a9075aa1b90260710fffb9a2b772246df567d478
Max Zimmermann <hobomojo3@gmail.com>
2024-12-30 23:15:03 -0500

    Parallax is (kinda) working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'personal-website';
+
+  public viewSideParallax = false;
+
+  toggleSideParallax() {
+    this.viewSideParallax = !this.viewSideParallax;
+  }
 }
