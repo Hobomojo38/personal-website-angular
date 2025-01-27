1382d6b1816ff9fadec71c1ab0277b4549794aec
Max Zimmermann <hobomojo3@gmail.com>
2025-01-27 18:58:20 -0500

    Added blog-component and touched up About Me section

---
+++ src/app/blog-landing-page/blog-landing-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-landing-page',
+  imports: [],
+  templateUrl: './blog-landing-page.component.html',
+  styleUrl: './blog-landing-page.component.css'
+})
+export class BlogLandingPageComponent {
+
+}
