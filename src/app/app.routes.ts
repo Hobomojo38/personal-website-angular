0f672c95117f58c7a7f9ee107c52f1af1c3a15fe
Max Zimmermann <hobomojo3@gmail.com>
2025-08-14 13:55:59 -0400

    fixing css attempt 9

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,26 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { BlogLandingPageComponent } from './blog-landing-page/blog-landing-page.component';
 import { BlogArticleComponent } from './blog-landing-page/blog-article/blog-article.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
-    {path: '', component: HomePageComponent},
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomePageComponent},
     {path: 'blog', component: BlogLandingPageComponent},
     { path: 'blog/:id', component: BlogArticleComponent },
-    // { path:"**", component: ErrorComponent }, // For developing a 404 page
+
+    // { path:"**", component: ErrorComponent }, // For developing a 404 page, keep it last
 ];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'enabled', // scroll to top on nav
+      anchorScrolling: 'enabled',           // support #anchor links
+      onSameUrlNavigation: 'reload'         // re-run guards/resolvers on same route
+    })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
